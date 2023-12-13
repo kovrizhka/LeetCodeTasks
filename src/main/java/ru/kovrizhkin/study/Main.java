23dca3fb7bdae14257109075236f2df116cd4f5b
kovrizhka <pivsru@gmail.com>
2023-12-13 18:16:49 +0500

    initial commit

--- src/main/java/ru/kovrizhkin/study/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.kovrizhkin.study;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/ru/kovrizhkin/study/solution1582/Solution.java
+++ src/main/java/ru/kovrizhkin/study/solution1582/Solution.java
@@ -1,4 +1,46 @@
 package ru.kovrizhkin.study.solution1582;
 
+
+/**
+ * Дана двоичная матрица , возвращаем количество специальных позиций в .m x nmatmat
+ * Позиция называется специальной, если и все остальные элементы в строке и столбце являются (строки и столбцы имеют индекс 0).(i, j)mat[i][j] == 1ij0
+ */
+
+
 public class Solution {
+    public int numSpecial(int[][] mat) {
+
+        int finalCount = 0;
+
+        for(int i = 0; i < mat.length; i++) {
+            boolean canWork = false;
+            int positionOfOneNumber = -1;
+
+            for(int j = 0; j < mat[i].length; j++) {
+                if(mat[i][j] == 1) {
+                    if (positionOfOneNumber == -1) {
+                        positionOfOneNumber = j;
+                        canWork = true;
+                    } else {
+                        canWork = false;
+                    }
+                }
+            }
+
+            if(canWork) {
+                int count = 0;
+                for(int k = 0; k < mat.length; k++) {
+                    if(mat[k][positionOfOneNumber] == 1 && k != i) {
+                        count++;
+                    }
+                }
+
+                if(count == 0) {
+                    finalCount++;
+                }
+            }
+
+        }
+        return finalCount;
+    }
 }
