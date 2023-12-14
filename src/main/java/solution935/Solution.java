905beb4fd6b511aecb672bf4208b3cc279d5f8f7
kovrizhka <pivsru@gmail.com>
2023-12-14 18:59:51 +0500

    initial commit

---
+++ src/main/java/solution935/Solution.java
@@ -0,0 +1,28 @@
+package solution935;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int[] sumCols = new int[grid[0].length];
+        int[] sumRows = new int[grid.length];
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == 0) {
+                    sumRows[i] += -1;
+                    sumCols[j] += -1;
+                } else {
+                    sumRows[i] += 1;
+                    sumCols[j] += 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = sumRows[i] + sumCols[j];
+            }
+        }
+        return grid;
+    }
+}
---
+++ src/main/java/solution935/Test.java
@@ -0,0 +1,10 @@
+package solution935;
+
+public class Test {
+    public static void main(String[] args) {
+        int [][] testArr = {{0,1,1},{1,0,1},{0,0,1}};
+        Solution solution = new Solution();
+
+
+    }
+}
